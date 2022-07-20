00fbb7aa7c9558c5cf2f6ffba78ce26dd8f07a00
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2022-07-20 17:22:39 +0100

    genearte latest music from spotify API

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -1,11 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import '../App.css'
 import Cards from "./Music-Card"
 
 export default function Body(){
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer BQCODFluloUgdquwIskAubq6N1oi0ruD9oJr88dM1YWgFHropZE-pfLnPFfmaPtzQWdgE0hwrPRLU2Ld_paGhkwpDywh9NecvvXdy2Pw_nZAj_csBfVCoD1VX9MPtlIKxs5g5ZW0Pd9fM9hkYvbA1hFUwkVkxb7uwM2ZBM8PhgPSLLKuoG8D8Hg9dkBC2zc");
+
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch("https://api.spotify.com/v1/browse/new-releases?country=US&limit=50&offset=5", requestOptions)
+        .then(response => response.json())
+        .then(result => setData(result.albums.items))
+        .catch(error => console.log('error', error))
+    },[])
+ 
+    const cardsData = data.map(data=> 
+    <Cards 
+    key={data.id}
+    image={data.images[0].url}
+    title={data.name}
+    artist={data.artists[0].name}
+    release = {data.release_date}
+    play={data.external_urls.spotify}
+    />)
+    console.log(data)
     return(
-        <main className="cards">
-            <Cards />
+        <main>
+            <div className="info">
+                <h4>Featured</h4>
+                <p className="new">New Release from Spotify</p>
+            </div>
+            <div className="cards">
+                {cardsData}
+            </div>
         </main>
     )
 }
--- src/Components/Music-Card.js
+++ src/Components/Music-Card.js
@@ -1,13 +1,18 @@
 import React from "react";
-import image from "../assets/music-display.png"
+import like from "../assets/like.png"
 
-export default function Cards(){
+export default function Cards(props){
     return (
         <div>
-            <img src={image} alt="music" className="music-image" />
+            <img src={props.image} alt="music" className="music-image" />
             <div className="music-details">
-                <h4 className="music-title">DO NOT DISTURB</h4>
-                <p className="artist">OMAH LAY</p>
+                <h4 className="music-title">{props.title}</h4>
+                <div className="title-and-like">
+                    <p className="artist">{props.artist}</p>
+                    <img src={like} alt="like"  className="like"/>
+                </div>
+                <p className="release_date">{props.release}</p>
+                <button className="play_Button"><a href={props.play}>Click to play</a></button>
             </div>
         </div>
     )
