6f28b7a1ae1dabd672f2fab9164d912682f05efc
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2022-07-21 14:15:52 +0100

    select favorite has been implemented

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -6,28 +6,29 @@ export default function Body(){
     const [data, setData] = useState([])
     useEffect(()=>{
         const myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer BQCODFluloUgdquwIskAubq6N1oi0ruD9oJr88dM1YWgFHropZE-pfLnPFfmaPtzQWdgE0hwrPRLU2Ld_paGhkwpDywh9NecvvXdy2Pw_nZAj_csBfVCoD1VX9MPtlIKxs5g5ZW0Pd9fM9hkYvbA1hFUwkVkxb7uwM2ZBM8PhgPSLLKuoG8D8Hg9dkBC2zc");
+        myHeaders.append("Authorization", "Bearer BQAVHd03sC09I4nwxPFIUTLAwPBAUIZz1s-pYA33SQjB8ncc5aOQDQpJV5muqe8-M6jHPV7DqYBZfqZRcA_dAOHaK4bEVTMF3enhqTooc7hFAXsZ1K16fJWLaGXkSB8jE71LxZhV-W7S9YSlDOQvFuNGSnf2DXQ-0EfHgLCW9OaoDH6iD0m829-GxxI5WnQ");
 
         const requestOptions = {
             method: 'GET',
             headers: myHeaders,
             redirect: 'follow'
         };
-
-        fetch("https://api.spotify.com/v1/browse/new-releases?country=US&limit=50&offset=5", requestOptions)
+        
+        fetch("https://api.spotify.com/v1/browse/new-releases?country=NG&limit=50&offset=5", requestOptions)
         .then(response => response.json())
         .then(result => setData(result.albums.items))
         .catch(error => console.log('error', error))
     },[])
- 
     const cardsData = data.map(data=> 
     <Cards 
     key={data.id}
+    id={data.id}
     image={data.images[0].url}
     title={data.name}
     artist={data.artists[0].name}
     release = {data.release_date}
     play={data.external_urls.spotify}
+    isFavorite = {true}
     />)
     console.log(data)
     return(
--- src/Components/Music-Card.js
+++ src/Components/Music-Card.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import like from "../assets/like.png"
+import favorite from "../assets/favorites.png"
 
 export default function Cards(props){
+    const[isLike, setIsLike] = useState({
+        isFavorite : false,
+        id:props.id
+    })
+    const [likedSong, setLikedSong] = useState([])   
+
+    function changeImage(){
+        isLike.isFavorite ? setIsLike(prevIsLike =>({
+            prevIsLike,
+            isFavorite:false
+        })) : setIsLike(prevIsLike =>({
+            prevIsLike,
+            isFavorite:true
+        }));
+    }
     return (
         <div>
             <img src={props.image} alt="music" className="music-image" />
@@ -9,7 +25,7 @@ export default function Cards(props){
                 <h4 className="music-title">{props.title}</h4>
                 <div className="title-and-like">
                     <p className="artist">{props.artist}</p>
-                    <img src={like} alt="like"  className="like"/>
+                    <img src={isLike.isFavorite ? favorite : like} alt="like"  className="like" onClick={changeImage}/>
                 </div>
                 <p className="release_date">{props.release}</p>
                 <button className="play_Button"><a href={props.play}>Click to play</a></button>
