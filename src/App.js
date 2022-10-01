2765c987348b01a709bf0f3ed94bf569753b1eff
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2022-10-01 17:46:18 +0100

    perf : improve performance for deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './Components/Home';
 import Quotes from './Components/Quotes';
 import QuotesContextProvider from './Context/QuotesContext';
 import MusicContextProvider from './Context/MusicContext';
-import EventContextProvider from './Context/EventContext'
+import EventContextProvider from './Context/EventContext.js'
 import FormContextProvider from './Context/FormContext';
 // import Collection from './Components/Collection'
 
